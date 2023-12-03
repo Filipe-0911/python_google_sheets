81c0d692697fc79198ef34bbd4b2af9749c357a3
Filipe <fbianchi.andrade@gmail.com>
2023-12-03 14:14:33 -0300

    adicionando nova classe tabela

--- interface.py
+++ interface.py
@@ -5,6 +5,7 @@ from tkinter import ttk
 from tkinter import *
 from ttkthemes import ThemedStyle
 from ttkthemes import ThemedTk
+from classes.Tabela import TabelaTkinter
 
 def on_button_click():
     get_gs_data()
@@ -28,12 +29,8 @@ def on_button_click():
     texto_completo = '\n\n'.join(texto_permutas)
     label2['text'] = texto_completo
 
-
-root = Tk()
-
-style = ThemedStyle(root)
-style.set_theme("blue")
-
+    
+root = tk.Tk()
 root.title("Permutas preenchidas")
 largura_tela = 800
 altura_tela = 1000
@@ -44,12 +41,12 @@ label_text = tk.StringVar()
 label_text.set("Permutas")
 
 label = Label(root, textvariable=label_text, wraplength=300, justify="center", font=("Helvetica", 12))
-label.pack(pady=10)
+label.grid(row=0, column=0, columnspan=3, pady=10)
 
-button = Button(root, text="Obter permutas", command=on_button_click)
-button.pack(pady=10)
+button = Button(root, text="Obter permutas", command=lambda: on_button_click())
+button.grid(row=1, column=0, columnspan=3, pady=10)
 
 label2 = Label(root, text='', wraplength=700, justify="center", font=("Helvetica", 10))
-label2.pack(pady=10)
+label2.grid(row=6, column=0, columnspan=3, pady=10)
 
 root.mainloop()
