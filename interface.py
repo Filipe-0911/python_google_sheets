194ef38da2c1d984f617266e2850a9f2976bbd1b
Filipe <fbianchi.andrade@gmail.com>
2023-12-05 18:04:39 -0300

    modificando interface.py

--- interface.py
+++ interface.py
@@ -4,7 +4,6 @@ from classes.PermutasCrud import PermutasCrud
 from tkinter import ttk
 from tkinter import *
 from ttkthemes import ThemedStyle
-#from ttkthemes import ThemedTk
 
 def on_button_click():
     get_permutas()
@@ -13,27 +12,10 @@ def on_button_click():
     conecta_permuta.conectar_banco()
     permutas = conecta_permuta.consultar_permutas()
 
-    print(permutas)
-
-    # texto_permutas = []
-
-    # for permuta in permutas:
-    #     texto_permuta = (
-    #         f"Data da troca: {permuta[1]} - "
-    #         f"Proponente: {permuta[2]} - "
-    #         f"Sai do turno: {permuta[3]} - "
-    #         f"Entra no turno: {permuta[4]} - "
-    #         f"Proposto: {permuta[5]} - "
-    #         f"Sai do turno: {permuta[6]} - "
-    #         f"Entra no turno: {permuta[7]} - "
-    #     )
-
-    #     texto_permutas.append(texto_permuta)
-
-    # texto_completo = '\n\n'.join(texto_permutas)
     texto_completo = 'Permutas cadastradas no banco!'
     label2['text'] = texto_completo
 
+    return permutas
 
 def on_button_sdia_click():
     get_sdias()
@@ -51,10 +33,8 @@ altura_tela = 1000
 
 root.geometry(f"{largura_tela}x{altura_tela}")
 
-# Adiciona uma cor de fundo
-root.configure(bg="#f0f0f0")  # Substitua pela cor desejada em formato hexadecimal
+root.configure(bg="#f0f0f0") 
 
-# Centraliza o conteúdo na tela
 largura_tela_real = root.winfo_screenwidth()
 altura_tela_real = root.winfo_screenheight()
 pos_x = (largura_tela_real - largura_tela) // 2
@@ -65,16 +45,15 @@ label_text = tk.StringVar()
 label_text.set("Permutas")
 
 label = Label(root, textvariable=label_text, font=("Helvetica", 16), bg="#f0f0f0")
-label.grid(row=0, column=0)
+label.pack()
 
 button_obter_permutas = Button(root, text="Obter permutas", command=on_button_click, font=("Helvetica", 12))
-button_obter_permutas.grid(row=1, column=0, pady=10)
+button_obter_permutas.pack(pady=10)
 
 button_obter_sdia = Button(root, text="Obter SDIA", command=on_button_sdia_click, font=("Helvetica", 12))
-button_obter_sdia.grid(row=1, column=2, pady=10)
+button_obter_sdia.pack(pady=10)
 
-# Adiciona wrap na label2 e uma cor de fundo
 label2 = Label(root, text='', font=("Helvetica", 8), wraplength=345, justify="center", bg="#f0f0f0")
-label2.grid(row=2, column=0, columnspan=2, pady=3)
+label2.pack(pady=3)
 
 root.mainloop()
